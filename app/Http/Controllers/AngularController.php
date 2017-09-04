813040e4e7cfbe930ec9d78804c58cac82297ebe
Oleh Borysenko <oleg.borysenko@morefromit.com>
2017-09-04 15:09:57 +0200

    angular get data

--- app/Http/Controllers/AngularController.php
+++ app/Http/Controllers/AngularController.php
@@ -8,14 +8,8 @@
 
 namespace App\Http\Controllers;
 
-
-use App\Project;
-use App\ProjectTechnicalStack;
-use App\TechnicalStack;
-use App\TechnicalStackGroup;
-use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Lang;
-use Illuminate\Support\Facades\Mail;
 use TCG\Voyager\Voyager;
 
 class AngularController extends Controller {
@@ -27,5 +21,28 @@ class AngularController extends Controller {
         $data['seo_description'] = Voyager::setting('seo_description', '');
         $data['seo_image'] = '/images/me.jpg';
       	return view('angular.main', $data);
+    }
+
+    /**
+     * getMyData
+     * @author Oleh Borysenko <oleg.borisenko@morefromit.com>
+     * @return Response
+     */
+    public function getData()
+    {
+        $locale = Lang::getLocale();
+        return response([
+            'locale' => $locale,
+            'fullName' => 'Oleh Borysenko',
+            'email' => Voyager::setting('email', 'oleh@bodev.pro'),
+            'birthday' => '22.10.1994',
+            'birthplace' => 'Dnipro, Ukraine',
+            'skype' => Voyager::setting('skype', 'olehstail'),
+            'position' => Voyager::setting('header_title', 'Software Developer'),
+            'subposition' => Voyager::setting('header_subtitle', ''),
+            'about_me_long' => Voyager::setting('about_me_long_' . $locale, ''),
+            'about_me_short' => Voyager::setting('about_me_short_' . $locale, ''),
+
+        ]);
     }
 }
