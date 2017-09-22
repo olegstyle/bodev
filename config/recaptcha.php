c14c0ebff5938bfdfc4e892f58418304d1c7ba26
Oleh Borysenko <oleg.borisenko@morefromit.com>
2017-09-22 20:04:24 +0200

    recaptcha is working!

---
+++ config/recaptcha.php
@@ -0,0 +1,66 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | API Keys
+    |--------------------------------------------------------------------------
+    |
+    | Set the public and private API keys as provided by reCAPTCHA.
+    |
+    | In version 2 of reCAPTCHA, public_key is the Site key,
+    | and private_key is the Secret key.
+    |
+    */
+    'public_key'     => env('RECAPTCHA_PUBLIC_KEY', '6Ldc9jAUAAAAAIM4BlQIHl4r5Hu7XqS6rkZkduL5'),
+    'private_key'    => env('RECAPTCHA_PRIVATE_KEY', '6Ldc9jAUAAAAAJSBbGsFbmyT2CScObBNLJUyQ-SE'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Template
+    |--------------------------------------------------------------------------
+    |
+    | Set a template to use if you don't want to use the standard one.
+    |
+    */
+    'template'    => '',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Driver
+    |--------------------------------------------------------------------------
+    |
+    | Determine how to call out to get response; values are 'curl' or 'native'.
+    | Only applies to v2.
+    |
+    */
+    'driver'      => 'curl',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Options
+    |--------------------------------------------------------------------------
+    |
+    | Various options for the driver
+    |
+    */
+    'options'     => [
+
+        'curl_timeout' => 1,
+        'curl_verify' => true,
+
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Version
+    |--------------------------------------------------------------------------
+    |
+    | Set which version of ReCaptcha to use.
+    |
+    */
+
+    'version'     => 2,
+
+];
