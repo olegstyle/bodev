38fb61fbeb6556113dd6cb0da8597781eb66d934
Oleh Borysenko <olegstyle1@gmail.com>
2018-10-15 16:34:29 +0200

    fix

--- app/Http/Controllers/DictionaryController.php
+++ app/Http/Controllers/DictionaryController.php
@@ -66,7 +66,7 @@ class DictionaryController extends Controller
 
     public function privacyPolicy(string $appName): View
     {
-        $appNameTranslated = preg_replace('/^dictionary\./i', '', trans('dictionary.' . $appName));
+        $appNameTranslated = preg_replace('/^dictionary\./i', '', trans('dictionary.' . preg_replace('/\./', '_', $appName)));
 
         return view('pages.privacy_policy', [
             'appName' => $appName,
--- resources/lang/ru/dictionary.php
+++ resources/lang/ru/dictionary.php
@@ -1,9 +1,9 @@
 <?php
 
 return [
-    'pro.bodev.fairytales' => 'Тысяча и одна сказка',
-    'pro.bodev.horrorstories' => 'Тысяча и один ужас',
-    'pro.bodev.diets' => 'Тысяча и одна диета',
-    'pro.bodev.dreambook' => 'Тысяча и одно сноведение',
-    'pro.bodev.crib' => 'Тысяча и одна шпаргалка',
+    'pro_bodev_fairytales' => 'Тысяча и одна сказка',
+    'pro_bodev_horrorstories' => 'Тысяча и один ужас',
+    'pro_bodev_diets' => 'Тысяча и одна диета',
+    'pro_bodev_dreambook' => 'Тысяча и одно сноведение',
+    'pro_bodev_crib' => 'Тысяча и одна шпаргалка',
 ];
